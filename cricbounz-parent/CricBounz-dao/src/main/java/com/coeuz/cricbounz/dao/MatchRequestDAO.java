610c31b8276a5afb333bcaaec87c1941c8b177a6
Cric Bounz <Cric Bounz>
2016-04-24 15:17:48 +0530

    Mari Match notofication changes

--- cricbounz-parent/CricBounz-dao/src/main/java/com/coeuz/cricbounz/dao/MatchRequestDAO.java
+++ cricbounz-parent/CricBounz-dao/src/main/java/com/coeuz/cricbounz/dao/MatchRequestDAO.java
@@ -2,9 +2,15 @@ package com.coeuz.cricbounz.dao;
 
 import java.sql.SQLException;
 import java.util.Date;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.coeuz.cricbounz.model.MatchDetails;
 import com.coeuz.cricbounz.model.MatchRequest;
 import com.coeuz.cricbounz.model.RequestNotifications;
 @Repository
@@ -13,6 +19,11 @@ public class MatchRequestDAO extends BaseDAO<MatchRequest, Integer>{
 	RequestNotificationDAO requestNotificationDAO;
 	@Autowired
 	RequestNotifications requestNotifications;
+	@Autowired
+	CreateMatchDetailsDAO createMatchDetailsDAO;
+	@Autowired
+	MatchDetails matchDetails;
+	
 	
 	@Autowired
 	public MatchRequestDAO(SessionFactory sessionFactory) {
@@ -20,26 +31,58 @@ public class MatchRequestDAO extends BaseDAO<MatchRequest, Integer>{
 		super.setSessionFactory(sessionFactory);
 	}
 	
-	public void saveMatchRequest(MatchRequest matchRequest) throws SQLException,NullPointerException{
+	public void saveMatchRequest(MatchRequest matchRequest) throws NullPointerException,SQLException,ClassCastException{
 		long matchreqID = saveAndRetrunUniqkey(matchRequest);
 		if(matchreqID>0 && requestNotifications!=null){
 			requestNotifications.setRequestID(matchreqID);
 			requestNotifications.setRequestStatus(matchRequest.getRequestStatus());
 			requestNotifications.setRequestType("MatchRequest");
-			requestNotifications.setNotifyToID(matchRequest.getMatchRequestID());
+			requestNotifications.setNotifyToID(matchRequest.getRequestedToTeam());
 			requestNotifications.setTimeStamp(new Date());
 			requestNotificationDAO.saveorUpdate(requestNotifications);
 		}
 	}
+	public MatchRequest getRequestMatchidDetails(long matchRequestId) {
+		MatchRequest matchRequest = null;
+		matchRequest = (MatchRequest) get(matchRequestId);
+		return matchRequest;
+	}
 	
-	public void actionOnMatchRequest(MatchRequest matchRequest)throws SQLException,NullPointerException{
+	public void actionOnMatchRequest(MatchRequest matchRequest)throws NullPointerException,SQLException,ClassCastException{
 		
-		
-	 	
-	 	
-	   	
+		if(matchRequest.getRequestStatus().equals("accepted")){
+			MatchRequest retrievedmatchRequest = getRequestMatchidDetails(matchRequest.getMatchRequestID());
+			if(retrievedmatchRequest.getOpponentTeamApprovalCount()<4){
+				retrievedmatchRequest.setOpponentTeamApprovalCount(retrievedmatchRequest.getOpponentTeamApprovalCount()+1);	
+				saveorUpdate(retrievedmatchRequest);
+			}else if(retrievedmatchRequest.getOpponentTeamApprovalCount()>=4){
+				matchDetails.setTeamAId(matchRequest.getRequestedByTeam());
+				matchDetails.setTeamBId(matchRequest.getRequestedToTeam());
+				matchDetails.setStatus("Sheduled");
+				matchDetails.setTimestamp(new Date());
+				createMatchDetailsDAO.save(matchDetails);
+				Session session = getSessionFactory().openSession();
+				Transaction transaction = session.beginTransaction();
+				String notificationHql="DELETE RequestNotifications r WHERE r.requestID="+matchRequest.getMatchRequestID()+" AND r.requestType='MatchRequest'";
+				Query notificationQuery = session.createQuery(notificationHql);
+				notificationQuery.executeUpdate();
+				session.flush();
+				transaction.commit();
+				session.close();
+				requestNotifications.setRequestID(matchRequest.getMatchRequestID());
+				requestNotifications.setRequestStatus("Sheduled");
+				requestNotifications.setRequestType("MatchRequest");
+				requestNotifications.setNotifyToID(matchRequest.getRequestedToTeam());
+				requestNotifications.setTimeStamp(new Date());
+				requestNotificationDAO.saveorUpdate(requestNotifications);
+			}
+					
+		}
 		
 	}
+	
+	
+	
 	
 	
 }
--- cricbounz-parent/CricBounz-model/src/main/java/com/coeuz/cricbounz/model/MatchDetails.java
+++ cricbounz-parent/CricBounz-model/src/main/java/com/coeuz/cricbounz/model/MatchDetails.java
@@ -15,8 +15,8 @@ public class MatchDetails {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name="ID")	
-	private long id;
+	@Column(name="matchID")	
+	private long matchID;
 	private long teamAId;
 	private long teamBId;
 	private String venue;
@@ -28,80 +28,150 @@ public class MatchDetails {
 	private String ballType;
 	private String scorer;
 	private String pitchType;
-	public long getId() {
-		return id;
+	/**
+	 * @return the matchID
+	 */
+	public long getMatchID() {
+		return matchID;
 	}
-	public void setId(long id) {
-		this.id = id;
+	/**
+	 * @param matchID the matchID to set
+	 */
+	public void setMatchID(long matchID) {
+		this.matchID = matchID;
 	}
+	/**
+	 * @return the teamAId
+	 */
 	public long getTeamAId() {
 		return teamAId;
 	}
+	/**
+	 * @param teamAId the teamAId to set
+	 */
 	public void setTeamAId(long teamAId) {
 		this.teamAId = teamAId;
 	}
+	/**
+	 * @return the teamBId
+	 */
 	public long getTeamBId() {
 		return teamBId;
 	}
+	/**
+	 * @param teamBId the teamBId to set
+	 */
 	public void setTeamBId(long teamBId) {
 		this.teamBId = teamBId;
 	}
+	/**
+	 * @return the venue
+	 */
 	public String getVenue() {
 		return venue;
 	}
+	/**
+	 * @param venue the venue to set
+	 */
 	public void setVenue(String venue) {
 		this.venue = venue;
 	}
+	/**
+	 * @return the status
+	 */
 	public String getStatus() {
 		return status;
 	}
+	/**
+	 * @param status the status to set
+	 */
 	public void setStatus(String status) {
 		this.status = status;
 	}
+	/**
+	 * @return the timestamp
+	 */
 	public Date getTimestamp() {
 		return timestamp;
 	}
+	/**
+	 * @param timestamp the timestamp to set
+	 */
 	public void setTimestamp(Date timestamp) {
 		this.timestamp = timestamp;
 	}
+	/**
+	 * @return the wonBy
+	 */
 	public String getWonBy() {
 		return wonBy;
 	}
+	/**
+	 * @param wonBy the wonBy to set
+	 */
 	public void setWonBy(String wonBy) {
 		this.wonBy = wonBy;
 	}
+	/**
+	 * @return the umpire1
+	 */
 	public String getUmpire1() {
 		return umpire1;
 	}
+	/**
+	 * @param umpire1 the umpire1 to set
+	 */
 	public void setUmpire1(String umpire1) {
 		this.umpire1 = umpire1;
 	}
+	/**
+	 * @return the umpire2
+	 */
 	public String getUmpire2() {
 		return umpire2;
 	}
+	/**
+	 * @param umpire2 the umpire2 to set
+	 */
 	public void setUmpire2(String umpire2) {
 		this.umpire2 = umpire2;
 	}
+	/**
+	 * @return the ballType
+	 */
 	public String getBallType() {
 		return ballType;
 	}
+	/**
+	 * @param ballType the ballType to set
+	 */
 	public void setBallType(String ballType) {
 		this.ballType = ballType;
 	}
+	/**
+	 * @return the scorer
+	 */
 	public String getScorer() {
 		return scorer;
 	}
+	/**
+	 * @param scorer the scorer to set
+	 */
 	public void setScorer(String scorer) {
 		this.scorer = scorer;
 	}
+	/**
+	 * @return the pitchType
+	 */
 	public String getPitchType() {
 		return pitchType;
 	}
+	/**
+	 * @param pitchType the pitchType to set
+	 */
 	public void setPitchType(String pitchType) {
 		this.pitchType = pitchType;
 	}
-	
-	
-	
+		
 
 }
